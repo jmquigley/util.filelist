e76b10e056bd16761648bf3fbb46c38e5d0f1c08
James Quigley <jmquigley@outlook.com>
2017-03-18 11:37:24 -0500

    Reorganized dependencies
    - Reverted tests.ts.  It can't use util.fixture or it creates a circular
    dependency between the two.  The util.fixture class needs to use this
    module.
    - Adds power assert to the project

--- test/tests.ts
+++ test/tests.ts
@@ -3,23 +3,39 @@
 import * as assert from 'assert';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import {Fixture} from 'util.fixture';
-import {debug} from './helpers';
+import * as uuid from 'uuid';
 import {getFileList} from '../index';
+
+const home = require('expand-home-dir');
+
+let unitTestBaseDir = home(path.join('~/', '.tmp', 'unit-test-data'));
+let unitTestDir = home(path.join(unitTestBaseDir, uuid.v4()));
+if (!fs.existsSync(unitTestDir)) {
+	fs.mkdirsSync(unitTestDir);
+}
+let f = path.join(unitTestDir, 'testfile.list');
 
 describe('Testing util.filelist', () => {
 
-	after(() => {
-		debug('final cleanup');
-		let directories = Fixture.cleanup();
-		directories.forEach((directory: string) => {
-			assert(!fs.existsSync(directory));
-		});
+	before(() => {
+		let lines = [
+			'# Comment line',
+			'',
+			'	item1',
+			'item2',
+			'',
+			'	item 3   '
+		];
+
+		fs.writeFileSync(f, lines.join('\n'));
+	});
+
+	after('cleanup', () => {
+		fs.removeSync(unitTestBaseDir);
 	});
 
 	it('Validating file list creation', () => {
-		let fixture = new Fixture('simple');
-		let lines = getFileList(path.join(fixture.dir, 'testfile.list'));
+		let lines = getFileList(f);
 
 		assert(lines instanceof Array);
 		assert(lines.length === 3);
