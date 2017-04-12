0134b1a301a603af263ab8b6660056362b89c6bb
James Quigley <jmquigley@outlook.com>
2017-04-12 10:15:03 -0500

    Reverts testing back to ava
    - Updates all dependencies

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@
 import * as fs from 'fs-extra';
 
 export function getFileList(filename: string) {
-	let arr: string[] = [];
+	const arr: string[] = [];
 
 	if (fs.existsSync(filename)) {
-		let lines = fs.readFileSync(filename).toString().split(/\r?\n|\r/);
+		const lines = fs.readFileSync(filename).toString().split(/\r?\n|\r/);
 
 		lines.forEach((line) => {
 			line = line.trim();
--- test/tests.ts
+++ test/tests.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as assert from 'assert';
+import test from 'ava';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as uuid from 'uuid';
@@ -8,39 +8,38 @@ import {getFileList} from '../index';
 
 const home = require('expand-home-dir');
 
-let unitTestBaseDir = home(path.join('~/', '.tmp', 'unit-test-data'));
-let unitTestDir = home(path.join(unitTestBaseDir, uuid.v4()));
+const unitTestBaseDir = home(path.join('~/', '.tmp', 'unit-test-data'));
+const unitTestDir = home(path.join(unitTestBaseDir, uuid.v4()));
 if (!fs.existsSync(unitTestDir)) {
 	fs.mkdirsSync(unitTestDir);
 }
-let f = path.join(unitTestDir, 'testfile.list');
+const f = path.join(unitTestDir, 'testfile.list');
 
-describe('Testing util.filelist', () => {
+test.before(t => {
+	let lines = [
+		'# Comment line',
+		'',
+		'	item1',
+		'item2',
+		'',
+		'	item 3   '
+	];
 
-	before(() => {
-		let lines = [
-			'# Comment line',
-			'',
-			'	item1',
-			'item2',
-			'',
-			'	item 3   '
-		];
+	fs.writeFileSync(f, lines.join('\n'));
+	t.pass();
+});
 
-		fs.writeFileSync(f, lines.join('\n'));
-	});
+test.after(t => {
+	fs.removeSync(unitTestBaseDir);
+	t.pass();
+});
 
-	after('cleanup', () => {
-		fs.removeSync(unitTestBaseDir);
-	});
+test('Validating file list creation', t => {
+	let lines = getFileList(f);
 
-	it('Validating file list creation', () => {
-		let lines = getFileList(f);
-
-		assert(lines instanceof Array);
-		assert(lines.length === 3);
-		assert.equal(lines[0], 'item1');
-		assert.equal(lines[1], 'item2');
-		assert.equal(lines[2], 'item 3');
-	});
+	t.true(lines instanceof Array);
+	t.is(lines.length, 3);
+	t.is(lines[0], 'item1');
+	t.is(lines[1], 'item2');
+	t.is(lines[2], 'item 3');
 });
